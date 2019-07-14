a34be221cf68f3ff220092671320d54b12e3d528
Michał Zieliński <zielu0@gmail.com>
2019-07-14 07:48:46 +0700

    [FIX] time for autoupdate currencies

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('currency:update')->everyMinute();
+        $schedule->command('currency:update')->daily();
     }
 
     /**
